ebc94e9c443ae57280b13528a2eb68348cecce5d
Koesob <epi3706@khu.ac.kr>
2022-01-11 17:44:43 +0900

    515p

--- Source/ArenaBattle/Public/ABCharacter.h
+++ Source/ArenaBattle/Public/ABCharacter.h
@@ -16,6 +16,8 @@ class ARENABATTLE_API AABCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AABCharacter();
+	void SetCharacterState(ECharacterState NewState);
+	ECharacterState GetCharacterState() const;
 
 protected:
 	// Called when the game starts or when spawned
@@ -42,7 +44,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void PostInitializeComponents() override;
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
-	virtual void PossessedBy(AController* NewController) override;
+	// virtual void PossessedBy(AController* NewController) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
@@ -113,6 +115,27 @@ private:
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
 		float AttackRadius;
 
-	FSoftObjectPath CharacterAssetToLoad = FSoftObjectPath(nullptr);
+	int32 AssetIndex = 0;
+
+	// FSoftObjectPath CharacterAssetToLoad = FSoftObjectPath(nullptr);
+	FStringAssetReference CharacterAssetToLoad;
+
 	TSharedPtr<struct FStreamableHandle> AssetStreamingHandle;
+
+	UPROPERTY(Transient, VisibleInstanceOnly, BlueprintReadOnly, Category = State, Meta = (AllowPrivateAccess = true))
+		ECharacterState CurrentState;
+
+	UPROPERTY(Transient, VisibleInstanceOnly, BlueprintReadOnly, Category = State, Meta = (AllowPrivateAccess = true))
+		bool bIsPlayer;
+
+	UPROPERTY()
+		class AABAIController* ABAIController;
+
+	UPROPERTY()
+		class AABPlayerController* ABPlayerController;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = State, Meta = (AllowPrivateAccess = true))
+		float DeadTimer;
+
+	FTimerHandle DeadTimerHandle = {};
 };
