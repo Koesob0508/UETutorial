ec3fab7f09aff7508a6da14070aff812da2336c3
Koesob <epi3706@khu.ac.kr>
2022-01-12 12:16:19 +0900

    608p

--- Source/ArenaBattle/Private/ABPlayerController.cpp
+++ Source/ArenaBattle/Private/ABPlayerController.cpp
@@ -5,6 +5,7 @@
 #include "ABHUDWidget.h"
 #include "ABPlayerState.h"
 #include "ABCharacter.h"
+#include "ABGameplayWidget.h"
 
 AABPlayerController::AABPlayerController()
 {
@@ -13,22 +14,44 @@ AABPlayerController::AABPlayerController()
 	{
 		HUDWidgetClass = UI_HUD_C.Class;
 	}
+
+	static ConstructorHelpers::FClassFinder<UABGameplayWidget> UI_MENU_C(TEXT("/Game/Book/UI/UI_Menu.UI_Menu_C"));
+	if (UI_MENU_C.Succeeded())
+	{
+		MenuWidgetClass = UI_MENU_C.Class;
+	}
+}
+
+void AABPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	InputComponent->BindAction(TEXT("GamePause"), EInputEvent::IE_Pressed, this, &AABPlayerController::OnGamePause);
 }
 
 void AABPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FInputModeGameOnly InputMode;
-	SetInputMode(InputMode);
+	ChangeInputMode(true);
 
 	HUDWidget = CreateWidget<UABHUDWidget>(this, HUDWidgetClass);
-	HUDWidget->AddToViewport();
+	ABCHECK(nullptr != HUDWidget);
+	HUDWidget->AddToViewport(1);
 
 	ABPlayerState = Cast<AABPlayerState>(PlayerState);
 	ABCHECK(nullptr != ABPlayerState);
 	HUDWidget->BindPlayerState(ABPlayerState);
 	ABPlayerState->OnPlayerStateChanged.Broadcast();
+}
+
+void AABPlayerController::OnGamePause()
+{
+	MenuWidget = CreateWidget<UABGameplayWidget>(this, MenuWidgetClass);
+	ABCHECK(nullptr != MenuWidget);
+	MenuWidget->AddToViewport(3);
+
+	SetPause(true);
+	ChangeInputMode(false);
 }
 
 UABHUDWidget* AABPlayerController::GetHUDWidget() const
@@ -44,6 +67,20 @@ void AABPlayerController::NPCKill(AABCharacter* KilledNPC) const
 void AABPlayerController::AddGameScore() const
 {
 	ABPlayerState->AddGameScore();
+}
+
+void AABPlayerController::ChangeInputMode(bool bGameMode)
+{
+	if (bGameMode)
+	{
+		SetInputMode(GameInputMode);
+		bShowMouseCursor = false;
+	}
+	else
+	{
+		SetInputMode(UIInputMode);
+		bShowMouseCursor = true;
+	}
 }
 
 void AABPlayerController::PostInitializeComponents()
--- Source/ArenaBattle/Public/ABPlayerController.h
+++ Source/ArenaBattle/Public/ABPlayerController.h
@@ -23,17 +23,30 @@ public:
 	class UABHUDWidget* GetHUDWidget() const;
 	void NPCKill(class AABCharacter* KilledNPC) const;
 	void AddGameScore() const;
+	void ChangeInputMode(bool bGameMode = true);
 
 protected:
+	virtual void SetupInputComponent() override;
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
 		TSubclassOf<class UABHUDWidget> HUDWidgetClass;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
+		TSubclassOf<class UABGameplayWidget> MenuWidgetClass;
+
 private:
+	void OnGamePause();
+
 	UPROPERTY()
 		class UABHUDWidget* HUDWidget;
 
 	UPROPERTY()
 		class AABPlayerState* ABPlayerState;
+
+	UPROPERTY()
+		class UABGameplayWidget* MenuWidget;
+
+	FInputModeGameOnly GameInputMode;
+	FInputModeUIOnly UIInputMode;
 };
